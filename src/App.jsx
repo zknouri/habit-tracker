f62de6ec1b79cf5f0a5991f02b0db71f04c880fd
Zakaria NAOURI <zakariaenouri@gmail.com>
2026-05-16 01:39:32 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { AddHabitForm } from "./components/AddHabitForm.jsx";
+import Header from "./components/Header.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AddHabitForm />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/AddHabitForm.jsx
@@ -0,0 +1,20 @@
+export function AddHabitForm() {
+  return (
+    <form className="bg-emerald-300 rounded-xl p-4 m-2">
+      <div className="flex gap-2 mb-4">
+        <label className="w-29 text-gray-700 cursor-text" htmlFor="habit">
+          Habit Name:
+        </label>
+        <input className="bg-gray-50 rounded-xl w-full" type="text" />
+      </div>
+      <div className="flex justify-end gap-2">
+        <button className="bg-red-400 hover:bg-red-500 rounded-xl w-16 p-2 cursor-pointer text-gray-50">
+          Cancel
+        </button>
+        <button className="bg-blue-400 hover:bg-blue-500 rounded-xl w-16 p-2 cursor-pointer text-gray-50">
+          Add
+        </button>
+      </div>
+    </form>
+  );
+}
